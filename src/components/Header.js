84c266c816f0f849e93b3421607f86f5ff6b2198
pangj130 <pangj@bu.edu>
2022-06-14 15:40:06 -0400

    pushing progress on kitten contact, still needs work

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,5 @@
 const Header = () => {
     return (
-        /* 
-        <div className = 'topnav'>
-            <a href = "https://github.com/pangj130"> Github </a>
-            <a href="#contact">Contact</a>
-            <a href="#about">Projects</a>
-            <a class="active" href="#home">About me</a>
-        </div>
-        */
        <div className = 'header'> 
            <text className = 'header_left'> Jonathan Pang </text>
            <text className = 'header_right'> Contact: pangj@bu.edu </text>
--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -3,8 +3,14 @@ import kittenContact from './KittenContact.png'
 const MyProjects = () => {
   return (
     <div>
-      <img src = {kittenContact} className = 'pic_one'/> 
-      <aside className = 'text_one_background'> </aside>
+      <img src = {kittenContact} className = 'pic_one' alt = 'Kitten Contact'/>
+      <div className = 'text_one_background'> 
+        <text>
+          Something
+        </text>
+        <a href = 'https://github.com/pangj130/Kitten-Gacha' className = 'link_one'>Codebase</a>
+      </div>
+      <img src = {kittenContact} className = 'pic_one_test' alt = 'Kitten Contact'/> 
     </div>
   )
 }
