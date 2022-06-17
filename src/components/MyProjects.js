44900ac05c00a1c492e51aaddfd909e1bbd456e4
pangj130 <pangj@bu.edu>
2022-06-17 01:04:18 -0400

    organized project one

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -2,15 +2,15 @@ import kittenContact from './KittenContact.png'
 
 const MyProjects = () => {
   return (
-    <div>
+    <div className = 'outer'>
       <img src = {kittenContact} className = 'pic_one' alt = 'Kitten Contact'/>
+      <img src = {kittenContact} className = 'pic_one_test' alt = 'Kitten Contact'/>
       <div className = 'text_one_background'> 
         <text>
           Something
         </text>
         <a href = 'https://github.com/pangj130/Kitten-Gacha' className = 'link_one'>Codebase</a>
       </div>
-      <img src = {kittenContact} className = 'pic_one_test' alt = 'Kitten Contact'/> 
     </div>
   )
 }
